f89544c1baa89c3cbae3e04a5578e423a14ca4f6
Shim Jae Chun <shimnaldo98@gmail.com>
2023-12-18 17:47:13 +0900

    초기 커밋

---
+++ CsharpSyntax/Program.cs
@@ -0,0 +1,22 @@
+﻿//namespace CsharpSyntax
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+            
+//        }
+//    }
+//    class Animal
+//    {
+//        protected string name;
+
+//    }
+//    class Cat : Animal
+//    {
+//        public void SetName() {
+//            Animal a = new Animal();
+//            Cat c = new Cat();
+//        }
+//    }
+//}

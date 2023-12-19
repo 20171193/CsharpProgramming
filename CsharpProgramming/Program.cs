d64e9bd2e9f4a5c4b4b325595ee4eaa508b9b01e
Shim Jae Chun <shimnaldo98@gmail.com>
2023-12-19 09:49:47 +0900

    1. 입력 및 출력

--- CsharpProgramming/Program.cs
+++ CsharpProgramming/Program.cs
@@ -1,33 +1,12 @@
-﻿using System;
+﻿//using System;
 
-// K칸 점프 = -K
-// 현재거리 * 2 = 0
-
-class Solution
-{
-    static void Main(string[] args)
-    {
-        Solution sol = new Solution(); 
-        Console.WriteLine($"answer = {sol.solution(5000)}");
-    }
-
-    public int solution(int n)
-    {
-        int[] dp = new int[n+1]; 
-        int answer = 0;
-        dp[0] = 0;
-        for (int i = 1; i <= n; i++)
-        {
-            if (i / 2 > 0 && i%2 == 0)
-            {
-                dp[i] = dp[i / 2];
-            }
-            else
-            {
-                dp[i] = dp[i - 1] + 1;
-            }
-        }
-        answer = dp[n];
-        return answer;
-    }
-}
+//namespace CsharpProgramming
+//{
+//    class Program
+//    {
+//        static void Main(string[] args) 
+//        {
+            
+//        }
+//    }
+//}
--- CsharpSyntax/Program.cs
+++ CsharpSyntax/Program.cs
@@ -1,22 +1,29 @@
-﻿//namespace CsharpSyntax
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-            
-//        }
-//    }
-//    class Animal
-//    {
-//        protected string name;
+﻿namespace CsharpSyntax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "기본 입출력";    // Console창의 제목을 설정
+            Console.BackgroundColor = ConsoleColor.DarkBlue;    // 텍스트 배경색 설정. 
+            Console.Write("문자를 입력하시오 : ");  // 문자열 출력
+            Console.ReadKey(); // ConsoleKeyInfo 구조체로 할당 
+            Console.WriteLine("\n입력성공");        // 문자열 출력 후 자동 줄바꿈
 
-//    }
-//    class Cat : Animal
-//    {
-//        public void SetName() {
-//            Animal a = new Animal();
-//            Cat c = new Cat();
-//        }
-//    }
-//}
+            Console.ResetColor();   // 콘솔 창에 설정된 배경색과 글자색을 기본 설정 값으로 변경.
+            Console.ForegroundColor = ConsoleColor.DarkGray;    // 텍스트 글자색 설정
+
+            string str = "";
+            Console.Write("\n문자열을 입력하고 엔터키를 누르시오 : ");
+            str = Console.ReadLine();   // string형 변수에 할당
+            Console.WriteLine($"입력한 문자열은 \"{str}\" 입니다.");      // 출력 방식 1
+            Console.WriteLine("입력한 문자열은 \"" + str + "\" 입니다."); // 출력 방식 2
+
+            Console.WriteLine("\n3초 후 콘솔창이 지워집니다.");
+            Thread.Sleep(3000); // 스레드를 3초간 일시 중단(밀리초 : 1/1000)
+            Console.Clear();    // 콘솔 창 내용 삭제
+            Console.WriteLine("1초 후 프로그램이 종료됩니다.");
+            Thread.Sleep(1000);
+        }
+    }
+}

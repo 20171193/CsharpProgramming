aedfd13e2c04171ddda22d3f9707674661dc2c41
Shim Jae Chun <shimnaldo98@gmail.com>
2023-12-20 13:45:28 +0900

    01. 입출력 수정

--- ETC/Program.cs
+++ ETC/Program.cs
@@ -1,10 +1,43 @@
 ﻿namespace ETC
 {
-    internal class Program
+    #region 입출력, 변수
+    //class Program
+    //{
+    //    struct Userinfo
+    //    {
+    //        public string name;
+    //        public string job;
+    //        public int level;
+    //        public int hp;
+    //    }
+    //    static void Main(string[] args)
+    //    {
+    //        Userinfo uinfo;
+    //        Console.WriteLine("\n  <캐릭터 선택창>\n"); ;
+    //        Console.Write("이름을 입력하세요 : ");
+    //        uinfo.name = Console.ReadLine();
+    //        Console.Write("직업을 입력하세요 : ");
+    //        uinfo.job = Console.ReadLine();
+    //        Console.Write("레벨을 입력하세요 : ");
+    //        uinfo.level = int.Parse(Console.ReadLine());
+    //        Console.Write("체력을 입력하세요 : ");
+    //        uinfo.hp = int.Parse(Console.ReadLine());
+
+    //        Console.WriteLine("\n선택하신 캐릭터는");
+    //        Console.WriteLine($"이름 : {uinfo.name}");
+    //        Console.WriteLine($"직업 : {uinfo.job}");
+    //        Console.WriteLine($"레벨 : {uinfo.level}");
+    //        Console.WriteLine($"체력 : {uinfo.hp}");
+    //    }
+    //}
+    #endregion
+
+    #region 연산자
+    class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) 
         {
-            Console.WriteLine("Hello, World!");
         }
     }
+    #endregion
 }

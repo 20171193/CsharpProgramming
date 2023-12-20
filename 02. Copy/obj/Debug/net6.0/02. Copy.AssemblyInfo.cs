b878bbfc94ba36ad39bb44bcecf38b770783e312
Shim Jae Chun <shimnaldo98@gmail.com>
2023-12-20 09:02:21 +0900

    폴더 정리

--- 02. Copy/obj/Debug/net6.0/02. Copy.AssemblyInfo.cs
+++ 02. Copy/obj/Debug/net6.0/02. Copy.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("etc")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("02. Copy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("etc")]
-[assembly: System.Reflection.AssemblyTitleAttribute("etc")]
+[assembly: System.Reflection.AssemblyProductAttribute("02. Copy")]
+[assembly: System.Reflection.AssemblyTitleAttribute("02. Copy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment 클래스에서 생성되었습니다.
